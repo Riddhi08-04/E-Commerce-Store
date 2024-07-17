ed8ec1c11244906a195a43cfcb5ed4e0fedb9c12
Riddhi Verma <133693241+Riddhi08-04@users.noreply.github.com>
2024-07-17 17:45:46 +0530

    Add files via upload

---
+++ src/component/layout/About/About.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./aboutSection.css";
+import { Button, Typography, Avatar } from "@material-ui/core";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import InstagramIcon from "@material-ui/icons/Instagram";
+const About = () => {
+  return (
+    <div className="aboutSection">
+      <div className="aboutSectionGradient"></div>
+      <div className="aboutSectionContainer">
+        <Typography component="h1">About Us</Typography>
+        <div>
+          <div>
+            <Typography>Riddhi Verma</Typography>
+          </div>
+          <div className="aboutSectionContainer2">
+            <Typography component="h2">Our Brands</Typography>
+            <a>
+              <YouTubeIcon className="youtubeSvgIcon" />
+            </a>
+           <a>
+              <InstagramIcon className="instagramSvgIcon" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
